b575b7a03b764d3676c1052680a57a565aa5d129
hunt <hunt.b.graham@gmail.com>
2015-01-04 18:59:13 -0500

    Added a FakeSink class.

---
+++ NuJack/FakeSink.java
@@ -0,0 +1,32 @@
+//
+//  Testing low level HiJack functionality.
+//
+
+package NuJack;
+
+import java.util.*;
+
+public class FakeSink implements IncomingSink {
+
+	List<Integer> lengths = new ArrayList<Integer>();
+	List<Boolean> trans = new ArrayList<Boolean>();
+	
+	public void handleNextBit(int transistionPeriod, boolean isHighToLow)
+	{
+		Integer i1 = new Integer(transistionPeriod);
+		Boolean i2 = new Boolean(isHighToLow);
+		lengths.add(i1);
+		trans.add(i2);
+	}
+
+	public void Print()
+	{
+		Integer i1 = new Integer(1);
+		lengths.add(i1);
+
+		for (Integer i : lengths)
+		{
+			System.out.println(i);
+		}
+	}
+}
