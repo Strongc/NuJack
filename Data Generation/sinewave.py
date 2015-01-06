ab7be228fba0fed6b8acfcfc74cae6e3298c1f08
hunt <hunt.b.graham@gmail.com>
2015-01-05 19:18:49 -0500

    Added accumlator function.

--- Data Generation/sinewave.py
+++ Data Generation/sinewave.py
@@ -1,26 +1,13 @@
 import math
 import matplotlib.pyplot as plt
 
-# Generate xCords. Pass num of points and period
-def generateXCords(num_points, period):
-    points = reversed(range(1, num_points))
-    xs = map (lambda x: 2*math.pi / x, points)
-    print '\n\nxs:\n' + str(xs)
-    ys = map (lambda x: 5000*math.sin(period * x), xs)
-    return zip (xs, ys)
-	
-def genCords(num_points):
-    points = reversed(range(2, num_points+1))
-    xs = map (lambda x: 2*math.pi * x/num_points, points)
-    print '\n\nxs:\n' + str(xs)
-    ys = map (lambda x: 5000*math.sin(1 * x), xs)
-    return zip (xs, ys)
-	
+# Generate cords. Pass num of points and period	
 def genCords2(num_points, period):
-    points = reversed(range(1, num_points+1))
-    xs = map (lambda x: period/2*math.pi * x/num_points, points)
-    print '\n\nxs:\n' + str(xs)
-    ys = map (lambda x: 5000*math.sin(period * x), xs)
+    points = reversed(range(0, num_points+1))
+    #xs = map (lambda x: period*2*math.pi * x/num_points, points)
+    xs = map (lambda x: 1*math.pi * x/num_points, points)
+    #print '\n\nxs:\n' + str(xs)
+    ys = map (lambda x: -5000*math.sin(2 * period * x), xs)
     return zip (xs, ys)
 
 # Pass period as 1/2 for long wave.
@@ -28,5 +15,20 @@ def getYCord( x, period ):
    return 5000*math.sin(math.pi * period * x)
    
 #print '\n\nres:\n' + str(generateXCords(100, 1))
-plt.plot(genCords2(100, 2))
+#plt.plot(genCords2(100, 2) + genCords2(100,1) + genCords2(100, 2))
+#plt.show()
+
+def accum(bits):
+    cords = []
+    for bit in bits:
+        if bit == 0:
+            cords = cords + genCords2(100,1)
+        else:
+            cords = cords + genCords2(100,2)
+    return cords
+
+#freq = accum([1,0,1])
+#print freq
+plt.plot(accum([1,0,1]))
 plt.show()
+
