dbdc634ac03fbf00c2d744e82e4b83606d5c42b1
hunt <hunt.b.graham@gmail.com>
2015-01-05 19:40:53 -0500

    Added writing to file.

--- Data Generation/sinewave.py
+++ Data Generation/sinewave.py
@@ -1,22 +1,12 @@
 import math
 import matplotlib.pyplot as plt
 
-# Generate cords. Pass num of points and period	
+# Generate cords. Pass number of points and period	multiplier
 def genCords2(num_points, period):
-    points = reversed(range(0, num_points+1))
-    #xs = map (lambda x: period*2*math.pi * x/num_points, points)
+    points = reversed(range(0, num_points+1))    
     xs = map (lambda x: 1*math.pi * x/num_points, points)
-    #print '\n\nxs:\n' + str(xs)
     ys = map (lambda x: -5000*math.sin(2 * period * x), xs)
     return zip (xs, ys)
-
-# Pass period as 1/2 for long wave.
-def getYCord( x, period ):
-   return 5000*math.sin(math.pi * period * x)
-   
-#print '\n\nres:\n' + str(generateXCords(100, 1))
-#plt.plot(genCords2(100, 2) + genCords2(100,1) + genCords2(100, 2))
-#plt.show()
 
 def accum(bits):
     cords = []
@@ -27,8 +17,13 @@ def accum(bits):
             cords = cords + genCords2(100,2)
     return cords
 
-#freq = accum([1,0,1])
-#print freq
+def writeToFile(data):
+    open('new_data.txt', 'w').write('')
+    for _,y in data:
+        f = open('new_data.txt', 'a')
+        f.write(str(math.floor(y)) + '\n')
+        
+#writeToFile( [(1,1), (2,2)] ) 
+writeToFile(accum([1,0,1]))
 plt.plot(accum([1,0,1]))
 plt.show()
-
